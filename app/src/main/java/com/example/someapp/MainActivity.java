72cfb5c8c3eed45416303f1b147c3402c4a52c2c
Lamanxo <smgbysgm@gmail.com>
2024-08-18 21:13:28 +0300

    work

--- app/src/main/java/com/example/someapp/MainActivity.java
+++ app/src/main/java/com/example/someapp/MainActivity.java
@@ -7,8 +7,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.someapp.adapters.CategoryAdapter;
+import com.example.someapp.adapters.CourseAdapter;
+import com.example.someapp.models.Category;
+import com.example.someapp.models.Course;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView categoryRecycler, courseRecycler;
+    private CategoryAdapter categoryAdapter;
+    private CourseAdapter courseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Игры"));
+        categoryList.add(new Category(1, "Сайты"));
+        categoryList.add(new Category(1, "Языки"));
+        categoryList.add(new Category(1, "Прочее"));
+
+        List<Course> coursesList = new ArrayList<>();
+        coursesList.add(new Course(1, "java_course", "Профессия Java\nразработчик", "13 января", "начальный", "#FF000000"));
+
+        setCourseRecycler(coursesList);
+        setCategoryRecycler(categoryList);
+    }
+
+    private void setCourseRecycler(List<Course> coursesList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        courseRecycler = findViewById(R.id.courseRecycler);
+        courseRecycler.setLayoutManager(layoutManager);
+        courseAdapter = new CourseAdapter(this, coursesList);
+        courseRecycler.setAdapter(courseAdapter);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
     }
 }
